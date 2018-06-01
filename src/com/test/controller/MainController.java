a72790e76e688d41d74317f7693f9e7e1a22bdb6
anderson222 <henoch.ndaya@gmail.com>
2018-06-01 03:33:41 -0400

    ajout methods greetings

--- src/com/test/controller/MainController.java
+++ src/com/test/controller/MainController.java
@@ -1,5 +1,11 @@
 package com.test.controller;
 
 public class MainController {
+	
+	private static final String HELLO = "programmeur";
+	
+	public String greetings(String hello) {
+		return HELLO + " " +hello;
+	}
 
 }
